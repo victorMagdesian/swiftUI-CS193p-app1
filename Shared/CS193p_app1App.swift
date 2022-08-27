5d93fa755a9eb96de79f7eb7096fae634d3542d0
victor <victor.magdesian@modal.com.br>
2022-08-27 13:34:17 -0300

    Initial Commit

---
+++ Shared/CS193p_app1App.swift
@@ -0,0 +1,17 @@
+//
+//  CS193p_app1App.swift
+//  Shared
+//
+//  Created by VICTOR FELIPPE MAGDESIAN on 27/08/22.
+//
+
+import SwiftUI
+
+@main
+struct CS193p_app1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
